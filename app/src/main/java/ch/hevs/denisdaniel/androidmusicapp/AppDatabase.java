79c676b22396ce52cdbe0d290bd16c8259c8c1dc
supimba <dnlro@DESKTOP-1JI2JIU.home>
2017-11-17 13:45:39 +0100

    -  AppDatabase, ajout DAO

--- app/src/main/java/ch/hevs/denisdaniel/androidmusicapp/AppDatabase.java
+++ app/src/main/java/ch/hevs/denisdaniel/androidmusicapp/AppDatabase.java
@@ -4,10 +4,13 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+
+import ch.hevs.denisdaniel.androidmusicapp.Albums.Albums.Album;
+import ch.hevs.denisdaniel.androidmusicapp.Albums.Albums.AlbumDao;
 import ch.hevs.denisdaniel.androidmusicapp.Artists.Artist;
 import ch.hevs.denisdaniel.androidmusicapp.Artists.ArtistDao;
-
-@Database(entities = {Artist.class}, version = 1)
+// TODO Have add new Entities Album.class
+@Database(entities = {Artist.class, Album.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase INSTANCE;
@@ -15,6 +18,8 @@ public abstract class AppDatabase extends RoomDatabase {
     public static final String DB_NAME = "app_db";
 
     public abstract ArtistDao artistDao();
+
+    public abstract AlbumDao albumDao();
 
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
